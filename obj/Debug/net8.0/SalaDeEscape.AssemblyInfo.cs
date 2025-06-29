b533c3576f1ac777bd28f9a7fafb02e98f22f4a6
Your Namejuaaloop <juana@lopezvalenzuela.com>
2025-06-29 13:04:37 -0300

    dejame pullear lpm git

--- obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab675da8c38733954cbd8dec24e6a54bd8dbf156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568b89248c405bf921697479da38546468c8ce85")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
