ab6c62672bfad4d21d75320bc2aacee00ddcb26b
juaaloop <juana@lopezvalenzuela.com>
2025-06-29 22:48:27 -0300

    wuu

--- obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
+++ obj/Debug/net8.0/SalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86eeb56eb651f2d06acda28be98bb920aa857d4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7417f682488b1846660fab48396c27f73186d278")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
